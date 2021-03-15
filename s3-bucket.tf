1310e4ca03fde282b9227a73590fedcd18839f55
Toni Tauro <eye@eyenx.ch>
2021-03-15 10:50:39 +0000

    fix(s3-bucket.tf): validation

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,5 @@
-source  = "app.terraform.io/chip-tf-training/s3-bucket/aws"
+module "s3-bucket" {
+  source  = "app.terraform.io/chip-tf-training/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
   bucket_prefix = "${var.prefix}"
